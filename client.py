f49767eeba0a0d69577f6c5e81d6b041fd99f9ea
ysoto025 <55642058+ysoto025@users.noreply.github.com>
2021-01-31 16:09:16 -0500

    Update client.py

--- client.py
+++ client.py
@@ -5,20 +5,24 @@ from sys import argv
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
+print (argv)
+if len(argv) < 4:
+    sys.exit()
 
-if len(argv) < 3:
-    print("missing arguments")
+if argv[1] == "":
     sys.exit()
-try:
-    socket.gethostbyname(argv[1])
-except socket.error:
-    sys.exit()
+else:
+    try:
+        socket.gethostbyname(argv[1])
+    except socket.error:
+        sys.exit()
 
 try:
     sock.connect((argv[1], int(argv[2])))
     print("It connected")
-except sock.gaierror as err:
-    print("Could not connect to host due error:" + err)
+except socket.error:
+    print("Could not connect to host due error:")
+    sys.exit()
 
 file = open(argv[3], "rb")
 
